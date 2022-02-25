4495d6b1be1bd74fc59d0ebeab09e526ee3aee43
Jacob Robinett <jacob.robinett@ou.edu>
2022-02-24 20:53:55 -0600

    about updated

--- src/components/modals/AboutModal.tsx
+++ src/components/modals/AboutModal.tsx
@@ -9,13 +9,19 @@ export const AboutModal = ({ isOpen, handleClose }: Props) => {
   return (
     <BaseModal title="About" isOpen={isOpen} handleClose={handleClose}>
       <p className="text-sm text-gray-500 dark:text-gray-300">
-        This is the open source version of the word guessing game we all know and
-        love - customized by {' '}
+        Jacob's version of an open source Wordle Clone - {' '}
         <a
           href="http://jacobrobinett.com"
           className="underline font-bold"
         >
           Jacob Robinett
+        </a>{' '}
+        {' '}
+        <a
+          href="https://github.com/cwackerfuss/react-wordle"
+          className="underline font-bold"
+        >
+        Orignal Repo
         </a>{' '}
       </p>
     </BaseModal>
